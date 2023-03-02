ea3214c968f26b5c1dff117b0744db295ce13264
rafaelvm <vieiramrafael@gmail.com>
2023-03-01 22:35:30 -0300

    feat: added layout import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Layout } from '@/layout/Layout.component'
 import { GlobalStyle } from './styles/global'
 import { theme } from './styles/theme'
 
-function App() {
+export function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <h2>App</h2>
+      <Layout />
     </ThemeProvider>
   )
 }
-
-export default App
