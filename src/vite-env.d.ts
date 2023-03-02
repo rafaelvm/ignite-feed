388e7de89e9cff0f31cd707a6ec4bcfd9d717353
rafaelvm <vieiramrafael@gmail.com>
2023-03-01 22:33:36 -0300

    feat: added phosphor icon and theme config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import { theme } from './styles/theme'
+
+export type ITheme = typeof theme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ITheme {}
+}
